af1d450efc591a3a43a053d5a016b2e7260b848c
Kal <kal.aalrajhi@gmail.com>
2022-03-30 17:36:44 -0600

    Branch out to work on whats-cookin alternative

---
+++ src/classes/RecipeRepository.js
@@ -0,0 +1,13 @@
+export class RecipeRepository {
+  constructor(recipe) {
+    this.recipe = recipe;
+  }
+
+  filterByTag(tag) {
+    // return a filtered set of recipes based off tag
+  }
+  
+  filterByName(name) {
+    // return a filtered set of recipes based off name
+  }
+}
---
+++ test/RecipeRepository-test.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import { RecipeRepository } from '../src/classes/RecipeRepository';
+
+describe('Recipe Repository', () => {
+    let recipeRepository;
+
+  beforeEach(() => {
+    recipeRepository = new RecipeRepository();
+
+  });
+
+  it('should be a function', () => {
+    expect(RecipeRepository).to.be.a('function');
+  });
+
+  it('should create a new instance of RecipeRepository', () => {
+    expect(recipeRepository).to.be.an.instanceof(RecipeRepository);
+  });
+
+})
